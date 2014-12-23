e532832e6cb83a950764404a80afed798ce62582
paloalto2014 <admin@panoramix.gateway.pace.com>
2014-12-22 18:29:52 -0800

    Initial commit, chapter 4 working

---
+++ rango/urls.py
@@ -0,0 +1,9 @@
+__author__ = 'admin'
+
+from django.conf.urls import patterns, url
+from rango import views
+
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+                       url(r'^about/', views.about, name='about'),
+)
